95d7158821ab42facd23c31c98d7a9d101f02c5c
pomelo1213 <goodmanluoxin@163.com>
2019-02-05 22:37:08 +0800

    slider

--- script/index.js
+++ script/index.js
@@ -1,4 +1,23 @@
 (function(){
   // console.log('this si run function')
-
+  let options = {
+    el: document.querySelector('#slider'),
+    sliders: [
+      {
+        link: '#',
+        image: '../images/go-2019.jpg'
+      },{
+        link: '#',
+        image: '../images/spring-night.jpg'
+      },{
+        link: '#',
+        image: '../images/time-place.jpeg'
+      },{
+        link: '#',
+        image: '../images/traditional.jpg'
+      }
+    ]
+  }
+  let slider = new Slider(options)
+  window.slider = slider
 })()
---
+++ script/slider.js
@@ -0,0 +1,39 @@
+class Slider {
+  constructor({el, sliders, interval}){
+    this.el = el
+    this.sliders = sliders
+    this.interval = interval | 1000
+    this.currentIndex = 0
+    // console.log(this.el)
+    this.render()
+    this.start()
+  }
+  render(){
+    this.el.innerHTML = `<ul class="slider-wrap"></ul>`
+    this.wrapEle = this.el.firstElementChild
+    this.wrapEle.style.width = `${this.sliders.length * 100}%`
+    this.wrapEle.innerHTML = this.sliders.map(slider => {
+      return `
+        <li class="slider-item">
+          <a href="${slider.link}">
+            <img src="${slider.image}">
+          </a>
+        </li>
+      `
+    }).join('')
+  }
+
+  start(){
+    setInterval(this.next.bind(this), this.interval)
+  }
+
+  next(){
+    if(this.currentIndex > this.sliders.length){
+      this.wrapEle.style.transform = 'translate(0)'
+      this.currentIndex = 0
+      return
+    }
+    this.wrapEle.style.transform = `translate(-${(this.currentIndex * 100) / this.sliders.length}%)`
+    this.currentIndex += 1
+  }
+}
