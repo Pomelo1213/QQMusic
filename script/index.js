252ddb1a9fa248d54ddce492dadc5622982260f8
pomelo1213 <goodmanluoxin@163.com>
2019-02-25 10:45:20 +0800

    添加hot-song

--- script/index.js
+++ script/index.js
@@ -23,9 +23,14 @@
 
   fetch('../json/recommand.json')
   .then(response => response.json())
-  .then(renderRadio)
+  .then(render)
+  // .then(renderRadio)
+  // .then(renderHotSongs)
+  function render(json){
+    renderRadio(json)
+    renderHotSongs(json)
+  }
   function renderRadio(json){
-    console.log('this is radio',json)
     let radioData = json.data.radioList
     let radioEle = document.querySelector('.radio-wrap')
     radioEle.innerHTML = radioData.map(radio =>
@@ -44,5 +49,20 @@
   }
 
   function renderHotSongs(json){
+    let hotSongsData = json.data.songList
+    let hotSongEle = document.querySelector('.hot-songs-wrap')
+    hotSongEle.innerHTML = hotSongsData.map(hotSong =>
+      `
+        <li class="hot-song-item">
+          <div class="item-media">
+            <img src="${hotSong.picUrl}">
+            <span class="icon icon-play"></span>
+          </div>
+          <div class="item-info">
+            <h3>${hotSong.songListDesc}</h3>
+          </div>
+        </li>
+      `
+    ).join('')
   }
 })()
